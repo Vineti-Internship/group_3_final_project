d2e81d28bf58c1dc7e140257d0f5a3699cb805a3
Edgar <amirbekianedgar@gmail.com>
2018-12-08 19:08:08 +0400

    add constraint not null

--- backend/db/migrate/20181208144640_create_sections.rb
+++ backend/db/migrate/20181208144640_create_sections.rb
@@ -1,7 +1,7 @@
 class CreateSections < ActiveRecord::Migration[5.2]
   def change
     create_table :sections do |t|
-      t.string :name
+      t.string :name, default: "", null:false
       t.references :course, foreign_key: true
       t.references :teacher, foreign_key: true
 
