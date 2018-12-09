912a78dd5d9a78968bd49318b837094422b4a706
Edgar <amirbekianedgar@gmail.com>
2018-12-09 18:39:41 +0400

    add sections - students many to many associations

--- backend/app/models/student.rb
+++ backend/app/models/student.rb
@@ -3,6 +3,8 @@ class Student < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Association with sections
+  has_and_belongs_to_many :sections
 
   # Add validators
   validates :first_name, :last_name, :password, :email, presence: true
