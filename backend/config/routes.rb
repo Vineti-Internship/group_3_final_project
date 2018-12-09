677fb40054b034538dbc0854f94dc5b960a70601
Edgar <amirbekianedgar@gmail.com>
2018-12-09 18:19:27 +0400

    add devise to teachers

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  # devise_for :teachers
+  # devise_for :students
   resources :teachers
   resources :students
   resources :sections
   resources :grades
   resources :exams
   resources :courses
+  # root to: "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
