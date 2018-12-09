76b8d85ca0fb9415ad13c49b762a5164d3033dda
Edgar <amirbekianedgar@gmail.com>
2018-12-10 00:39:28 +0400

    add rack-cors configuration

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -31,5 +31,14 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add Rack-Cors configurations
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete, :patch, :put]
+      end
+    end
+
   end
 end
