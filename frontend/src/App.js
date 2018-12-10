13a0f80326a7f6848f8796f9131f7d97fa74e40f
artur <artarmmush@gmail.com>
2018-12-10 13:42:18 +0400

    Remove unnecessary elements, and implement initial construction of project.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {LoginPage} from "./pages/loginPage";
+import {HomePage} from "./pages/homePage";
+import {PrivateRoute} from "./components/privateRoute";
+import {NotFoundPage} from "./pages/notFoundPage";
+import {store} from "./helpers/store";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    localStorage.setItem('user', JSON.stringify('{"name":"Artur"}'));
+    console.log('store ', store.getState());
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <BrowserRouter>
+          <Switch>
+            <PrivateRoute exact path="/" component={HomePage} />
+            <Route exact path="/login" component={LoginPage}/>
+            <Route path="*" component={NotFoundPage}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
