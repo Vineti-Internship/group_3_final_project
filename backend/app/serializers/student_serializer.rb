70dc724ba9ae7023a6a6dfc626687d19b9e94903
Edgar <amirbekianedgar@gmail.com>
2018-12-10 19:16:06 +0400

    fix associations in serializer

--- backend/app/serializers/student_serializer.rb
+++ backend/app/serializers/student_serializer.rb
@@ -1,5 +1,5 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :password, :email, :authentication_token
-  has_and_belongs_to_many :sections
+  has_many :sections
 
 end
