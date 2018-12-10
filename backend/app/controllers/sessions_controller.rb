601b32df8dca71eff1ef81eb325da07c6ce86d2e
artur <artarmmush@gmail.com>
2018-12-10 20:06:40 +0400

    Add require params permission.

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,17 +1,22 @@
 class SessionController < ApplicationController
 
-    def create
-        @user = Student.where(email: params[:email]).first || Teacher.where(email: params[:email]).first
+  def create
+    @user = Student.where(email: params[:email]).first || Teacher.where(email: params[:email]).first
 
-        if user&.valid_password?(params[:password])
-            render json: user
-        else
-            head(:unauthorized)
-        end
-
+    if user&.valid_password?(params[:password])
+      render json: user
+    else
+      head(:unauthorized)
     end
 
-    def destroy
-        current_user.authentication_token = nil
-    end
+  end
+
+  def destroy
+    current_user.authentication_token = nil
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
