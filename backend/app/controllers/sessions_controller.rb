14c8f7e6539a1417584ec0f002c8f414abc8ab98
Edgar <amirbekianedgar@gmail.com>
2018-12-10 20:53:10 +0400

    fix session controller

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionController < ApplicationController
   def create
     @user = Student.where(email: params[:email]).first || Teacher.where(email: params[:email]).first
 
-    if user&.valid_password?(params[:password])
-      render json: user
+    if @user&.valid_password?(params[:password])
+      render json: @user
     else
       head(:unauthorized)
     end
