82073e34c38a7333e79c01530b498ffd6c6ebd75
Edgar <amirbekianedgar@gmail.com>
2018-12-12 13:15:07 +0400

    add admin to serializers

--- backend/app/serializers/student_serializer.rb
+++ backend/app/serializers/student_serializer.rb
@@ -1,5 +1,5 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :password, :email, :authentication_token
+  attributes :id, :first_name, :last_name, :password, :email, :authentication_token, :admin
   has_many :sections
 
 end
--- backend/app/serializers/teacher_serializer.rb
+++ backend/app/serializers/teacher_serializer.rb
@@ -1,4 +1,4 @@
 class TeacherSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :password, :email, :authentication_token
+  attributes :id, :first_name, :last_name, :password, :email, :authentication_token, :admin
   has_one :section
 end
