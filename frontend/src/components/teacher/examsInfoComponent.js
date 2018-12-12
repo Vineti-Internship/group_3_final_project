abb88e20e696bb14c273b8bdd4734ee7489dbaa8
artur <artarmmush@gmail.com>
2018-12-12 23:38:31 +0400

    Add addExam functionality.

--- frontend/src/components/teacher/examsInfoComponent.js
+++ frontend/src/components/teacher/examsInfoComponent.js
@@ -4,24 +4,41 @@ import Fab from "@material-ui/core/es/Fab/Fab";
 import AddIcon from '@material-ui/icons/Add';
 import Grid from '@material-ui/core/Grid';
 import {ExamsLists} from "./examsLists";
+import {popupActions} from "../../ations/popupAction";
+import connect from "react-redux/es/connect/connect";
+import {ADD_EXAM_POPUP} from "../../constants/popupConstants";
 
-export const ExamsInfo = (props) => {
-  return (
-    <>
-      <Grid container >
-        <Typography variant="h5" color="inherit">
-          Exams Info
-        </Typography>
-        <Fab size="small" onClick={addExamClickHandler} color="primary" aria-label="Add">
-          <AddIcon/>
-        </Fab>
-      </Grid>
+class ExamsInfo extends React.Component {
 
-      <ExamsLists {...props}/>
-    </>
-  );
-};
+  addExamClickHandler = () => {
+    this.props.dispatch(popupActions.open(ADD_EXAM_POPUP));
+  };
 
-const addExamClickHandler = () => {
-  //should open popup to add date and topic of exam......................
-};
+  render() {
+    return (
+      <>
+        <Grid container>
+          <Typography variant="h5" color="inherit">
+            Exams Info
+          </Typography>
+          <Fab size="small" onClick={this.addExamClickHandler} color="primary" aria-label="Add">
+            <AddIcon/>
+          </Fab>
+        </Grid>
+
+        <ExamsLists {...this.props}/>
+      </>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const {isOpened, popupType} = state.popup;
+  return {
+    isOpened,
+    popupType
+  };
+}
+
+const connectedExamsInfo = connect(mapStateToProps)(ExamsInfo);
+export {connectedExamsInfo as ExamsInfo};
