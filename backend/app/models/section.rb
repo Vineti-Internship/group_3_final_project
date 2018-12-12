0c4077ddba37a4fa5d4fd9aa8da0fcade7b51af0
Edgar <amirbekianedgar@gmail.com>
2018-12-12 13:51:43 +0400

    remove name uniqueness validation

--- backend/app/models/section.rb
+++ backend/app/models/section.rb
@@ -4,5 +4,5 @@ class Section < ApplicationRecord
   has_and_belongs_to_many :students, dependent: :destroy
 
   # Add validators
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 end
