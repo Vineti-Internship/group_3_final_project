324b751b19b90b8f314d589e25dbb5c2f137dbc0
Edgar <amirbekianedgar@gmail.com>
2018-12-13 12:56:18 +0400

    fix admin serializer

--- backend/app/serializers/admin_serializer.rb
+++ backend/app/serializers/admin_serializer.rb
@@ -1,9 +1,3 @@
 class AdminSerializer < ActiveModel::Serializer
   attributes :id, :nickname
-  has_many :courses
-  has_many :exams
-  has_many :grades
-  has_many :sections
-  has_many :students
-  has_many :teachers
 end
