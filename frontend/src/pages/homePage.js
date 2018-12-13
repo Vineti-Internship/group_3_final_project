cbc7773ef74ecf07d29b4d173908b123844c1512
artur <artarmmush@gmail.com>
2018-12-13 14:13:59 +0400

    Fix error (appear after merge conflict solving).

--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import connect from "react-redux/es/connect/connect";
-<<<<<<< HEAD
 import TeacherView from "../components/teacher/teacherView";
-=======
-import {TeacherView} from "../components/teacher/teacherView";
->>>>>>> temporary comment student view import
+// temporary comment student view import...............................
 // import {StudentView} from "../components/students/StudentView";
 
 class HomePage extends React.Component{
