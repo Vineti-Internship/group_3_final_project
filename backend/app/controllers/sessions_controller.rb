2426b72cc2b74859e539de83dd12953bdac444ab
Edgar <amirbekianedgar@gmail.com>
2018-12-14 02:02:06 +0400

    fix session serializer 402 response

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   # user = Student.where(email: params[:email]).first || Teacher.where(email: params[:email]).first || Admin.where(email: params[:email]).first
 
   def create
+    
     user = Student.where(email: params[:email]).first || Teacher.where(email: params[:email]).first || Admin.where(email: params[:email]).first
 
     @user = user
@@ -20,12 +21,12 @@ class SessionsController < ApplicationController
 
     @user = user
     @user.authentication_token = nil
-    render json: @user
+    render json: @user.authentication_token
   end
 
   private
   # Only allow a trusted parameter "white list" through.
   def session_params
-    params.require(:session).permit(:email, :password)
+    params.permit(:email, :password)
   end
 end
