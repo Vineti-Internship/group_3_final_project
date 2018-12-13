ff134d0ddfc3cf9060d70bc0baf1f27f1ef0ed17
artur <artarmmush@gmail.com>
2018-12-13 19:51:10 +0400

    Create adminView, StudentView components (Templates).

--- frontend/src/pages/homePage.js
+++ frontend/src/pages/homePage.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import connect from "react-redux/es/connect/connect";
-import TeacherView from "../components/teacher/teacherView";
-// temporary comment student view import...............................
-// import {StudentView} from "../components/students/StudentView";
+//import TeacherView from "../components/teacher/teacherView";
+// import StudentView from "../components/student/studentView";
+// import AdminView from "../components/admin/adminView";
 
 class HomePage extends React.Component{
   render() {
     //should render a component depending on user type....................
     return (
       <div>
-        <TeacherView />
-        {/* <StudentView /> */}
+        {/*<TeacherView />*/}
+         {/*<StudentView />*/}
         {/*<AdminView />*/}
       </div>
     );
