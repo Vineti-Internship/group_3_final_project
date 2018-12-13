bc7600b21a59b8ff1ed55df050da33b5704655b7
Edgar <amirbekianedgar@gmail.com>
2018-12-13 13:05:01 +0400

    add password and email to admin controller's data permition

--- backend/app/controllers/admins_controller.rb
+++ backend/app/controllers/admins_controller.rb
@@ -46,6 +46,6 @@ class AdminsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def admin_params
-      params.require(:admin).permit(:nickname)
+      params.require(:admin).permit(:nickname, :password, :email)
     end
 end
