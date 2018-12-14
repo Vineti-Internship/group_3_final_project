9feab91762014c3ea0e3101008d447afc52ca7d3
Edgar <amirbekianedgar@gmail.com>
2018-12-14 12:42:40 +0400

    add session routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   resources :grades
   resources :exams
   resources :courses
-  resources :sessions, only: [:create, :destroy]
-  # post '/sessions' to: 'sessions#create'
-  # delete '/session/:email' 
+  # resources :sessions, only: [:create, :destroy]
+  post '/sessions', to: 'sessions#create'
+  post '/sessions/del', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
