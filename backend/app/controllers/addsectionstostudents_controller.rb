891ffe95dd153ac458bd2c9d5f640375a9e4c50e
Edgar <amirbekianedgar@gmail.com>
2018-12-16 00:54:14 +0400

    fix the controller of section insertion into student

--- backend/app/controllers/addsectionstostudents_controller.rb
+++ backend/app/controllers/addsectionstostudents_controller.rb
@@ -1,31 +1,22 @@
 class AddsectionstostudentsController < ApplicationController
-    before_action :set_student, only: [:create, :destroy]
+    before_action :set_user, only: [:create, :destroy]
   
     # POST /students/1
     def create
-      @student = Student.sections.append(Section.find(id = sectiontostudent_params)
-  
-      if @student.save
-        render json: @student.sections, status: :created, location: @student.sections
-      else
-        render json: @student.errors, status: :unprocessable_entity
-      end
+      @userio.sections.append(Section.find(id = request.raw_post()))
     end
   
     # DELETE /students/1
     def destroy
-      @student.sections = []
+      @userio.sections = []
     end
   
     private
+
       # Use callbacks to share common setup or constraints between actions.
-      def set_student
-        @student = Student.find(params[:id])
+      def set_user
+        @userio = Student.find(params[:id])
       end
   
-      # Only allow a trusted parameter "white list" through.
-      def student_params
-        params.permit(:first_name, :last_name, :password, :email, :admin_id)
-      end
   end
   
